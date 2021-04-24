875ddab6ef0dc7c8d8199d03a63b98100a55dd72
sirdedz <darbyedwards1@gmail.com>
2021-04-24 21:04:29 +0800

    Implementing basic forms funciton

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 (Fix later)
 Instructions for running flask app:
-1. Set FLASK_APP=sharkstakes.py
+1. FLASK_APP=sharkstakes.py
+2. export SECRET_KEY='<key>'
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
 from app import app 
+from flask import render_template, flash, redirect
+from app import forms
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title="SharkStakes")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = forms.LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    
+    return render_template('login.html', title="Sign In", form=form)
+
+
+@app.route('/register')
+def register():
+    return "Placeholder for registration page"
