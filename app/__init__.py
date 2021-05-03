a98d4b2c8ef019d4b9b7b0e69fa515223d181ebd
sirdedz <darbyedwards1@gmail.com>
2021-05-03 16:41:57 +0800

    Database functionality added to index page

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+#app.config.from_object('config.DevelopmentConfig') #use for changing config types
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- db_test.py
+++ db_test.py
@@ -1,8 +1,14 @@
 from app import db
-from app.models import User
+from app.models import User, Account, Offer, Event
 
 u = User(username="JohnDoe123", email='johndoe123@gmail.com')
+u2 = User(username="SamKent123", email='samk123@gmail.com')
+a = Account(balance=2000, user_id=1)
+a2 = Account(balance=5, user_id=2)
 db.session.add(u)
+db.session.add(u2)
+db.session.add(a)
+db.session.add(a2)
 db.session.commit()
 
 
