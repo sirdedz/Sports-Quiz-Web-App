9b0b2046f11f18369fe6d1c14e5a16a05bc90d76
sirdedz <darbyedwards1@gmail.com>
2021-05-17 17:29:39 +0800

    fixed login bug

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,3 @@
-#structure and routing for the website
-#routes pages forms and tables
-
 from app import app, db, models, forms
 from flask import render_template, flash, redirect, url_for, request, session
 from flask_login import current_user, login_user, logout_user, login_required
@@ -13,7 +10,6 @@ from datetime import datetime
 import json
 import populate_db
 
-#route first page as the index page
 @app.route('/')
 @app.route('/index')
 def index():
@@ -24,28 +20,25 @@ def index():
     return render_template('index.html', quizzes=quizzes)
 
 
-#sets login form on the page login.html
-#redirects depending on login form validators
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = forms.LoginForm()
 
     if not current_user.is_authenticated:
         if form.validate_on_submit():
-            UserController.login_post(form)
+            return UserController.login_post(form)
 
         return UserController.login(form)
 
 
     return redirect(url_for('user'))
 
-#routes functionality for logging out the user
 @app.route('/logout')
 def logout():
     return UserController.logout()
 
 
-#places RegistrationForm() on page register.html
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = forms.RegistrationForm()
@@ -56,8 +49,6 @@ def register():
     return UserController.register(form)
 
 
-#sets chosen quiz to render on quiz.html through quiz_title key
-#also routes questions through quiz_id key
 @app.route('/quiz/<string:title>', methods=['GET'])
 @login_required
 def quiz(title):
@@ -69,17 +60,14 @@ def quiz(title):
 
     return render_template('quiz.html', title="Quiz", quiz=quiz, questions=questions)
 
-#generates result controller for results page
 @app.route('/results', methods=['GET', 'POST'])
 @login_required
 def results():
     return ResultController.generate()
 
-#functionality for receiving and storing results into table
-#then show user final results on page
 @app.route('/get_results_json')
 def get_results_json():
-
+    
     results = Result.query.filter(Result.user_id==current_user.id).all()
     final = []
 
@@ -89,8 +77,6 @@ def get_results_json():
 
     return json.dumps(final, default=str)
 
-#adds functionality for admin
-#shows user results
 @app.route('/user', methods=['GET', 'POST'])
 @login_required
 def user():
@@ -101,23 +87,18 @@ def user():
 
     return render_template('user.html', title="Results")
 
-#render about page, no forms
 @app.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template('about.html', title="About Us")
 
-#render content page
 @app.route('/content', methods=['GET', 'POST'])
 def content():
     return render_template('content.html', title="Learning Materials")
 
-#get Statscontroller
 @app.route('/stats', methods=['GET'])
 def stats():
     return StatsController.get()
 
-#generate quiz for admin
-#must be logged in
 @app.route('/create_quiz', methods=['GET', 'POST'])
 @login_required
 def create_quiz():
@@ -132,9 +113,6 @@ def create_quiz():
     else:
         return redirect('index')
 
-#must be logged in
-#admin functionality
-#create quiz question and render through create_question.html
 @app.route('/create_question/<string:quiz_title>', methods=['GET', 'POST'])
 @login_required
 def create_question(quiz_title):
@@ -154,28 +132,25 @@ def create_question(quiz_title):
     else:
         return redirect('index')
 
-#admin function for removing a quiz from table/s
+
 @app.route('/delete_quiz/<string:title>', methods=['GET', 'POST'])
 @login_required
 def delete_quiz(title):
     if current_user.username == 'admin':
 
         quiz_object = Quiz.query.filter_by(title=title).first()
-        
         quiz_id = quiz_object.id
 
         db.session.query(Question).filter(Question.quiz_id==quiz_id).delete()
         db.session.query(Result).filter(Result.quiz_title==quiz_object.title).delete()
-
+        
         db.session.delete(quiz_object)
         db.session.commit()
 
-
+    
     return redirect('/')
 
-#must be logged in as user
-#retrieve corresct answers and compare to the user's
-#finalise user's score and store new_result
+
 @app.route('/submit_quiz', methods=['GET', 'POST'])
 @login_required
 def submit_quiz():
@@ -198,9 +173,9 @@ def submit_quiz():
         if m == u:
             #Correct answer
             score += 1
-
+        
         result[marking_questions[x].question] = [marking_questions[x].answer, jsonResult[x+1]['answer']]
-
+            
         questions_answered += 1
 
 
@@ -209,10 +184,10 @@ def submit_quiz():
 
     db.session.add(new_result)
     db.session.commit()
-
+    
     return json.dumps(result, default=str)
 
-#used to populate data tables for testing
+
 @app.route('/populate')
 @login_required
 def populate():
